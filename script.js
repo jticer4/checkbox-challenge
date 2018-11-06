5a5bbd6b0f055aea330f0f7c2cc3dd978bc6aaa7
jticer4 <jticer@cnm.edu>
2018-11-05 18:22:52 -0700

    Created a new variable lastChecked to store the value of the previously checked check box. I then set lastChecked equal to the event passed into the handleCheck function. I then added an if statement to the handleCheck function that will check to see if the shift key is being held down and the box is being checked.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //grab my checkboxes
 const checkboxes = document.querySelectorAll('.inbox input[ type="checkbox"]');
 
+let lastChecked;
+
 //control the checkbox functionality
 function handleCheck(e) {
-	console.log(e);
+	
+	//check to see if the shift key is held down and the box is being checked and not unchecked
+	if(e.shiftKey && this.checked){
+		//functionality enabling users to shift-click multiple checkboxes goes here
+	}
+	lastChecked = this;
 }
 
 //add event listeners to each checkbox
