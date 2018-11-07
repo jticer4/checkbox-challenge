e33db79254bb4fedc2db3200a774540070046455
jticer4 <jticer@cnm.edu>
2018-11-06 18:16:37 -0700

    Created a new variable called inBetween to store the checkboxes in between the two boxes that are selected. I then added a console log statement to test my if statement's functionality.

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ let lastChecked;
 
 //control the checkbox functionality
 function handleCheck(e) {
-	
+
+	let inBetween = false;
+
 	//check to see if the shift key is held down and the box is being checked and not unchecked
 	if(e.shiftKey && this.checked){
 		//functionality enabling users to shift-click multiple checkboxes goes here
+		//loop over all checkboxes
+		checkboxes.forEach(checkbox =>{
+			console.log(checkbox);
+		});
 	}
 	lastChecked = this;
 }
